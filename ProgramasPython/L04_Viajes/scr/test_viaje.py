69bc244de2b1b730148c9ff23f4f198d9d7c698f
José Manuel GV <jmgv.ciencia@gmail.com>
2024-10-16 02:14:53 +0200

    Mejora el código e implementa una nueva función con su test en viaje.py y test_viaje.py

--- ProgramasPython/L04_Viajes/scr/test_viaje.py
+++ ProgramasPython/L04_Viajes/scr/test_viaje.py
@@ -11,6 +11,7 @@ def test_ciudades_distintas(datos:str)->None:
     print(f"Las distintas ciudades a las que se puede viajar son: {ciudades_distintas(datos)}")
 
 def test_visitan_en_fecha(datos:str,ciudad:str,fecha:date=None)->None:
+    # No lo he hecho exactamente como pone en el REAME pero así me parece más reutilizable
     print("\ntest_visitan_en_fecha")
     viajesFiltrados = viajes_visitan_en_fecha(datos,ciudad,fecha)
     print("Los viajes que",end="" )
@@ -20,10 +21,21 @@ def test_visitan_en_fecha(datos:str,ciudad:str,fecha:date=None)->None:
         print(f" están de viaje el {fecha} y visitan {ciudad}",end="")
     print(f" son: {viajesFiltrados}")
 
-def test_viajes_que_visitan_más_ciudades(datos:str):
+def test_viajes_que_visitan_más_ciudades(datos:str)->None:
     print("\ntest_viajes_que_visitan_más_ciudades")
     filtrado = viajes_que_visitan_más_ciudades(datos)
-    print(filtrado)
+    for i in filtrado:
+        print(i)
+
+def test_n_viajes_más_importe(datos:str)->None:
+    print("\ntest_n_viajes_más_importante")
+    año_salida = 2025
+    viajes = n_viajes_más_importe(datos,año_salida)
+    print(f"Los cuatro viajes de más importe del {año_salida} son: {viajes}")
+    año_salida = 2024
+    n = 5
+    viajes = n_viajes_más_importe(datos,año_salida,n=5)
+    print(f"\nLos {n} viajes de más importe del {año_salida} son: {viajes}")
 
 if __name__ == "__main__":
     viajes = lee_viajes("ProgramasPython/L04_Viajes/data/viajes.csv")
@@ -32,3 +44,4 @@ if __name__ == "__main__":
     test_visitan_en_fecha(viajes,'Berlín')
     test_visitan_en_fecha(viajes,'Roma',date(2024,8,15))
     test_viajes_que_visitan_más_ciudades(viajes)
+    test_n_viajes_más_importe(viajes)
--- ProgramasPython/L04_Viajes/scr/viaje.py
+++ ProgramasPython/L04_Viajes/scr/viaje.py
@@ -1,5 +1,5 @@
 from typing import NamedTuple,List,Tuple
-from datetime import datetime,date
+from datetime import datetime,date,timedelta
 import csv
 
 Viaje = NamedTuple('viaje',[('código',str),('fec_sal',date),('fec_reg',date),('ciudades',List[str]),('num_per',int),('ppp',float),('seguro',bool)])
@@ -39,10 +39,16 @@ def viajes_visitan_en_fecha(datos:List[Viaje],ciudad:str,fecha:date=None)->None:
     Recibe una lista de tuplas de tipo Viaje, una ciudad y una fecha, que puede tomar el valor **None**
     Devuelve una lista con los registros que estarán de viaje en la fecha dada, incluyendo también visitar la ciudad que se indique. Si la fecha toma el valor **None** no se tendrán en cuenta la fecha.
     """
+    acum = []
+    for dato in datos:
+        if ciudad in dato.ciudades and (fecha == None or dato.fec_sal <= fecha <= dato.fec_reg):
+            acum.append(dato)
+    return acum
+""" Otra forma de hacerlo:
     datos = filter(lambda a: ciudad in a.ciudades, datos)
     if fecha != None:
         datos = filter(lambda a: a.fec_sal <= fecha and fecha <= a.fec_reg,list(datos))
-    return list(datos)
+    return list(datos)"""
 
 def viajes_que_visitan_más_ciudades(datos:List[Viaje])->List[Tuple[str,str,List[str]]]:
     """
@@ -60,5 +66,12 @@ def viajes_que_visitan_más_ciudades(datos:List[Viaje])->List[Tuple[str,str,List
         acum.append((código, f_sal, f_reg, ciudades))
     return acum
 
-def n_viajes_más_importe():
-    pass
+def n_viajes_más_importe(datos:List[Viaje],año_salida:int,n:int=4)->List[Tuple[str,timedelta,int,float,float]]:
+    """
+    Recibe una lista de tuplas de tipo Viaje, un año de salida y un entero "n" con valor por defecto 4
+    Devuelve una lista de tuplas con el código del viaje, los días de duración del viaje, el número de viajeros, el precio por persona y el importe total. 
+    La lista deberá estar ordenada de mayor a menor importe total.
+    """
+    filtro_year = [dato for dato in datos if dato.fec_sal.year == año_salida]
+    ordenado_importe = sorted(filtro_year,key=lambda a: a.num_per * a.ppp,reverse=True)[:n]
+    return [(viaje.código, (viaje.fec_reg-viaje.fec_sal).days, viaje.num_per, viaje.ppp, viaje.num_per * viaje.ppp) for viaje in ordenado_importe]
