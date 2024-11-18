ce58f89364c55cc4aaef040d537383385b3fa873
0_josema_0 <jmgv.ciencia@gmail.com>
2024-11-18 19:16:27 +0100

    <Do> Termina entero L07_CarrerasFormula1

--- ProgramasPython/L07_CarrerasFormula1/src/f1.py
+++ ProgramasPython/L07_CarrerasFormula1/src/f1.py
@@ -1,4 +1,4 @@
-from typing import NamedTuple,List,Union
+from typing import NamedTuple,List,Union,Optional,DefaultDict,Tuple,Dict
 from datetime import date,datetime
 import csv
 
@@ -22,3 +22,70 @@ def lee_carreras(nombre_fichero:str)->List[Carrera]:
             res.append(Carrera(nombre,escudería,fecha_carrera,temperatura_min,vel_max,duración,posición_final,ciudad,top_6_vueltas,tiempo_boxes,nivel_liquido))
     return res
             
+def media_tiempo_boxes(carreras:List[Carrera],c:str,f:Optional[date]=None)->Union[int,float]:
+    res = list()
+    for carrera in carreras: 
+        if (f == None or f == carrera.fecha_carrera) and c == carrera.ciudad:
+            res.append(carrera.tiempo_boxes)
+    return sum(res)/len(res) if len(res)>0 else int(0)
+
+def pilotos_menor_tiempo_medio_vueltas_top(carreras:List[Carrera],n:int)->Tuple[str,date]:
+    res = []
+    for carrera in carreras:
+        if -1 != carrera.posición_final: #omitimos quienes no completaron las 6 vueltas
+            media = sum(carrera.top_6_vueltas)/len(carrera.top_6_vueltas)
+            res.append( (carrera.nombre,carrera.fecha_carrera,media) )
+    n_mejores = sorted(res,key=lambda a: a[-1])[:n] 
+    return [(nombre,fecha) for nombre,fecha,_ in n_mejores]
+
+def ratio_tiempo_boxes_total(carreras:List[Carrera])->List[Tuple[str,date,float]]:
+    tiempo_total = DefaultDict(float)
+    for carrera in carreras:
+        tiempo_total[carrera.fecha_carrera] += carrera.tiempo_boxes
+    return sorted([(carrera.nombre,carrera.fecha_carrera,carrera.tiempo_boxes/tiempo_total[carrera.fecha_carrera]) for carrera in carreras],key=lambda a: a[-1],reverse=True)
+
+def mejor_escuderia_año(carreras:List[Carrera],a:int)->str:
+    victorias = dict()
+    for carrera in carreras:
+        if carrera.fecha_carrera.year == a:
+            if carrera.escudería not in victorias:
+                victorias[carrera.escudería] = 0
+            if carrera.posición_final == 1:
+                victorias[carrera.escudería] += 1
+    return max(victorias.items(),key=lambda a: a[1])[0]
+
+
+def puntos(carrera:Carrera)->float:
+    """
+    Recibe una carrera.
+    Devuelve los puntos obtenidos en esa carrera.
+    """
+    posición = carrera.posición_final
+    if posición == 1:
+        return 50
+    elif posición == 2:
+        return 25
+    elif posición == 3:
+        return 10
+    else:
+        return 0
+        
+def puntos_año(carreras:List[Carrera])->Dict[date,float]:
+    """
+    Recibe una lista de carreras.
+    Devuelve un diccionario con claves un año y con valor los puntos de ese año
+    """
+    res = DefaultDict(int)
+    for carrera in carreras:
+        res[carrera.fecha_carrera.year] += puntos(carrera)
+    return res
+
+def puntos_piloto_años(carreras:List[Carrera])->Dict[str,List[int]]:
+    pilotos = DefaultDict(list)
+    for carrera in carreras:
+        pilotos[carrera.nombre].append(carrera)
+    resultado = dict()
+    for c,v in pilotos.items():
+        puntos_por_año = puntos_año(v)
+        resultado[c] = [p for _,p in puntos_por_año.items()]
+    return resultado
--- ProgramasPython/L07_CarrerasFormula1/src/test_f1.py
+++ ProgramasPython/L07_CarrerasFormula1/src/test_f1.py
@@ -8,7 +8,43 @@ def test_lee_carreras(datos:List[Carrera])->None:
     for c in datos[:2]:
         print(f"\t{c}")
 
+def test_media_tiempo_boxes(datos:List[Carrera])->None:
+    print("\ntest_media_tiempo_boxes")
+    ciudad = "Barcelona"
+    print(f"La media de tiempo de boxes en la ciudad de {ciudad} es de {media_tiempo_boxes(datos,ciudad)} segundos.")
+    ciudad = "Barcelona"
+    fecha = date(2023,5,7)
+    print(f"La media de tiempo de boxes en la ciudad de {ciudad} es de {media_tiempo_boxes(datos,ciudad,fecha)} segundos.")
+    ciudad = "Lepe"
+    print(f"La media de tiempo de boxes en la ciudad de {ciudad} es de {media_tiempo_boxes(datos,ciudad)} segundos.")
+
+def test_pilotos_menor_tiempo_medio_vueltas_top(datos:List[Carrera])->None:
+    print("\ntest_pilotos_menor_tiempo_medio_vueltas_top")
+    n = 4
+    print(f"Los {n} pilotos con menos tiempo medio son {pilotos_menor_tiempo_medio_vueltas_top(datos,n)}")
+
+def test_ratio_tiempo_boxes_total(datos:List[Carrera])->None:
+    print("\ntest_ratio_tiempo_boxes_total")
+    tuplas = ratio_tiempo_boxes_total(datos)
+    for nom,fech,ratio in tuplas:
+        print(f"({nom}, {fech.strftime("%Y-%m-%d")}, {ratio:.3f})")
+
+def test_mejor_escuderia_año(datos:List[Carrera])->None:
+    print("\ntest_mejor_escuderia_año")
+    año = 2022
+    print(f"La mejor escudería en el año {año} ha sido {mejor_escuderia_año(datos,año)}.")
+
+def test_puntos_piloto_años(datos:List[Carrera])->None:
+    print("\ntest_puntos_piloto_años")
+    diccionario = puntos_piloto_años(datos)
+    for c,v in diccionario.items():
+        print(f"{c} --> {v}")
 
 if __name__ == "__main__":
     carreras = lee_carreras("ProgramasPython/L07_CarrerasFormula1/data/f1.csv")
     test_lee_carreras(carreras)
+    test_media_tiempo_boxes(carreras)
+    test_pilotos_menor_tiempo_medio_vueltas_top(carreras)
+    test_ratio_tiempo_boxes_total(carreras)
+    test_mejor_escuderia_año(carreras)
+    test_puntos_piloto_años(carreras)
